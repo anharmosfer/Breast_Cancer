2d1e79c4102d63984a837b6e923e57e350a31251
AnharMosfer <anharmosfer30@gmail.com>
2024-03-10 21:42:47 +0300

    save

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('city');
+            $table->date('birthdate');
+            $table->integer('age');
+            $table->enum('gender',['ذكر','أنثى']);
+            $table->enum('marital_status',['متزوج','غير متزوج']);
             $table->rememberToken();
             $table->timestamps();
         });

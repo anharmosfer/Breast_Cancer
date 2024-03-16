de14a58c93a6c28b1bb00ae3cb04ff39e8b39675
AnharMosfer <anharmosfer30@gmail.com>
2024-03-16 22:20:47 +0300

    save

--- database/migrations/2024_03_16_191704_create__answers_text_table.php
+++ database/migrations/2024_03_16_191704_create__answers_text_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('_answers_text', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            
         });
     }
 

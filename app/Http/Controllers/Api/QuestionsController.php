13e79049629181e97bde4b9dfb928581e8eaed45
AnharMosfer <anharmosfer30@gmail.com>
2024-03-30 05:37:21 +0300

    last

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreQuestionsRequest;
 use Illuminate\Http\Request;
+use App\Models\Questions;
 
 class QuestionsController extends Controller
 {
-    //
+    public function store(StoreQuestionsRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $question = new Questions([
+            'name' => $validatedData['name'],
+            'gender' => json_encode($validatedData['gender']),
+            'marital_status' => json_encode($validatedData['marital_status']),
+            'birthdate' => json_encode($validatedData['birthdate']),
+            'rate' => $validatedData['rate'],
+        ]);
+
+        $question->save();
+
+        return response()->json(['message' => 'Question created successfully'], 201);
+    }
 }
